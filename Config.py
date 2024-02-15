a19f73efd9d886da69705af3e68e43e6b1293e8f
NET_RUN_NER <120058097+Ranger0806@users.noreply.github.com>
2024-02-15 18:12:07 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = '6374688261:AAEHPkpT06dTrBu07_muKx3TXjvcSymM9vM'
+BOT_TOKEN = ''
 books = {"prose": "реалистическая проза", "detective": "детектив", "advanture": "приключенческая литература",
          "lover": "любовный роман", "povest": "повесть", "story": "рассказ",
          "historyrome": "исторический роман", "fantasy": "фантастика и фэнтези", "farytails": "сказки"}
